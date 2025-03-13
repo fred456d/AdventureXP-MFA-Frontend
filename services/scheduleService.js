9f4078b501f72d27b1038eca09f3522376c09470
marcus <marcus.bstroem@gmail.com>
2025-03-13 23:02:24 +0100

    kl slæt vist rigtig

--- services/scheduleService.js
+++ services/scheduleService.js
@@ -11,7 +11,7 @@ export async function saveInstructor(event) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ instructor: instructorName }),
+            body: JSON.stringify(instructorName),
         });
 
         if (!response.ok) {
