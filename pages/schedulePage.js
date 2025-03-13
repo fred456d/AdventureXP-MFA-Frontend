3d3552bd416aac9726913373b553e48e5490e0a5
marcus <marcus.bstroem@gmail.com>
2025-03-13 16:07:49 +0100

    reset vagtplan, ny løsning

--- pages/schedulePage.js
+++ pages/schedulePage.js
@@ -6,31 +6,30 @@ export async function schedulePage() {
     document.querySelector("#content").innerHTML = `
         <h1>Vagtplan</h1>
         <div id="scheduleContainer">
-            ${daysOfWeek.map(day => `
-                    <h2>${day}</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Tidspunkt</th>
-                                <th>Varighed</th>
-                                <th>Antal deltagere</th>
-                                <th>Aktivitet</th>
-                                <th>T-shirt/Soda/Slik</th>
-                                <th>Instruktør</th>
-                            </tr>
-                        </thead>
-                        <tbody id="${day}-table"></tbody>
-                    </table>
-                `).join('')}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Dato</th>
+                        <th>Tidspunkt</th>
+                        <th>Varighed</th>
+                        <th>Antal deltagere</th>
+                        <th>Aktivitet</th>
+                        <th>T-shirt/Soda/Slik</th>
+                        <th>Instruktør</th>
+                    </tr>
+                </thead>
+                <tbody id="scheduleTableBody"></tbody>
+            </table>
         </div>
     `;
+
 
 
     await loadSchedule();
 }
 
 async function loadSchedule() {
-    const tableBody = document.getElementById('Mandag-table');
+    const tableBody = document.getElementById('scheduleTableBody');
     tableBody.innerHTML = '';
 
     try {
@@ -39,10 +38,12 @@ async function loadSchedule() {
             const row = document.createElement('tr');
             const time_split = booking.time.split(":");
             const duration_split = booking.duration.split(":");
+            const date_split = booking.date.split("-");
 
             const instructor = booking.instructor || "-";
 
             row.innerHTML = `
+                <td>${date_split[2]}/${date_split[1]}</td>
                 <td>kl. ${time_split[0]}:${time_split[1]}</td>
                 <td>${duration_split[0]}t : ${duration_split[1]}m</td>
                 <td>${booking.participants}</td>
